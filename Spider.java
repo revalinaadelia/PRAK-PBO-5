e3952097dde685167ad3d042ff0507fbd26df372
revalinaadelia <144590332+revalinaadelia@users.noreply.github.com>
2024-10-03 17:39:22 +0700

    Spider.java

---
+++ Spider.java
@@ -0,0 +1,12 @@
+public class Spider extends Animal {
+
+    public Spider() {
+        super(8);
+        System.out.println("Saya adalah laba laba.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Sedang makan serangga.");
+    }
+}
